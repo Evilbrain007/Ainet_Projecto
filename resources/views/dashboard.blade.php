e019b7a4b12fe0aec65315bfaa96f871efafd662
Daniel Gomes <gomesdanielm@gmail.com>
2017-04-22 17:09:01 +0100

    Added master and dashboard blades.

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,89 @@
+@extends('master')
+
+@section('title', "PrintIT - pedidos de impressão")
+
+@section('navbar')
+    <button type=button" class="btn btn-danger navbar-btn" href="home.html">Login</button>
+@endsection
+
+@section('content')
+    <h1 class="page-header">
+        Página inicial
+    </h1>
+    </div>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-3">Total de impressões</div>
+            <div class="col-md-1">P/B</div>
+            <div class="col-md-1">Cor</div>
+            <div class="col-md-3">300 Impressoes hoje</div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">1280371</div>
+            <div class="col-md-1">77%</div>
+            <div class="col-md-1">23%</div>
+            <div class="col-md-3">Média imp. diárias Abril: 100</div>
+        </div>
+
+        <div class="table-responsive col-md-5">
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th>Departamento</th>
+                    <th>Total impressões</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                <tr>
+                    <td class="text-left">Departamento Leiria</td>
+                    <td class="text-right">100</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Departamento Abrantes</td>
+                    <td class="text-right">100</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Departamento Entroncamento</td>
+                    <td class="text-right">100</td>
+                </tr>
+                </tbody>
+            </table>
+
+        </div>
+
+        <div class="col-md-3">
+        </div>
+
+        <div class="table-responsive col-md-4">
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Telefone</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                <tr>
+                    <td class="text-left">Jose Migalha</td>
+                    <td class="text-right">jmig@cmosl.com</td>
+                    <td class="text-right">91222222</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Jose Migalha</td>
+                    <td class="text-right">jmig@cmosl.com</td>
+                    <td class="text-right">91222222</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Jose Migalha</td>
+                    <td class="text-right">jmig@cmosl.com</td>
+                    <td class="text-right">91222222</td>
+                </tr>
+                </tbody>
+            </table>
+
+        </div>
+@endsection
+
---
+++ resources/views/master.blade.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    <!-- Bootstrap 3 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Bootstrap 3 optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<div class="navbar navbar-fixed-top navbar-inverse">
+    @yield('navbar')
+</div>
+
+<div class="container">
+    @yield('content')
+</div>
+
+    <!-- JQuery  -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Bootstrap 3 JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossorigin="anonymous"></script>
+</body>
+</html>
