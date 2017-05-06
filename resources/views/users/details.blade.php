9dbbe094a4e1b92765d8436824a518a5232e27b2
Miguel <miguelsantos500@gmail.com>
2017-05-06 15:41:20 +0100

    UserDtails now uses master correctly. Added Stub Info.

--- resources/views/users/details.blade.php
+++ resources/views/users/details.blade.php
@@ -1,4 +1,4 @@
-@extends('master')
+@extends('layouts.master')
 
 @section('title')
 
@@ -8,10 +8,47 @@
 
 @section('content')
 
-    <h1>Detalhes do Utilizador</h1>
     <div>
         <h2>Nome:</h2>
         <h2></h2>
     </div>
+
+    <div class=" col-md-9 col-lg-9 ">
+        <table class="table table-user-information">
+            <tbody>
+            <tr>
+                <td>Department:</td>
+                <td>Programming</td>
+            </tr>
+            <tr>
+                <td>Hire date:</td>
+                <td>06/23/2013</td>
+            </tr>
+            <tr>
+                <td>Date of Birth</td>
+                <td>01/24/1988</td>
+            </tr>
+
+            <tr>
+            <tr>
+                <td>Gender</td>
+                <td>Female</td>
+            </tr>
+            <tr>
+                <td>Home Address</td>
+                <td>Kathmandu,Nepal</td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><a href="mailto:info@support.com">info@support.com</a></td>
+            </tr>
+            <td>Phone Number</td>
+            <td>123-4567-890(Landline)<br><br>555-4567-890(Mobile)
+            </td>
+
+            </tr>
+
+            </tbody>
+        </table>
 
 @endsection
