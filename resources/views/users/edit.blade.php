982ad06b1487132663b4300e03cf80326ac8854b
Daniel Gomes <gomesdanielm@gmail.com>
2017-05-11 11:54:02 +0100

    fixed merge

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -130,7 +130,7 @@
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
                                     <button type="submit" class="btn btn-primary">
-                                        Registar
+                                        Editar
                                     </button>
                                 </div>
                             </div>
--- routes/web.php
+++ routes/web.php
@@ -13,53 +13,36 @@
 Route::get('/', 'DashboardController@getIndex')->name('home');
 
 
-Route::get('/requests', function () {
-    return 'Lista de Pedidos de Impressão';
-});
+Route::get('/departament/{id}', 'DepartmentController@detail')->name('departmentDetail');
 
-Route::get('/request/{id}', function () {
-    return 'Detalhe de um Pedido';
-});
 
-Route::get('/requests/edit/{id}', 'RequestController@edit')->middleware('auth')->name('editRequest');
+
+Route::get('users/image/{user_id}', 'UserController@getUserImage')->name('getUserImage');
+
+Route::get('/users/edit/{id}', 'UserController@edit')->middleware('auth')->name('editUser');
+
+Route::post('/users/edit/{id}', 'UserController@update')->middleware('auth')->name('updateUser');
+
+Route::get('/user/{id}', 'UserController@details')->name('userDetail');
+
+
+Route::get('/requests/dashboard', 'RequestController@dashboard')->middleware('auth')->name('requestsDashboard');
 
 Route::get('/requests/create', 'RequestController@create')->middleware('auth')->name('createRequest');
 
 Route::post('/requests/create', 'RequestController@store')->middleware('auth');
-Route::post('/requests/edit/{id}', 'RequestController@update')->name('updateRequest');
 
-Route::get('/requests/create', 'RequestController@create')->name('createRequest');
+Route::get('/requests/edit/{id}', 'RequestController@edit')->middleware('auth')->name('editRequest');
+
+Route::post('/requests/edit/{id}', 'RequestController@update')->middleware('auth')->name('updateRequest');
 
 Route::get('/request/{id}', 'RequestController@details')->middleware('auth')->name('requestDetails');
 
+
 Route::post('requests/comments/create', 'RequestsController@createComment')->middleware('auth')->name('createComment');
-
-Route::get('/requests/dashboard', 'RequestController@dashboard')->middleware('auth')->name('requestsDashboard');
-
-Route::post('/requests/update/{id}', function () {
-    return 'Editar informação de um pedido';
-})->middleware('auth');
-
-Route::post('/requests/delete/{id}', function () {
-    return 'Eliminar informação de um pedido';
-})->middleware('auth');
-
-
-Route::get('/departament/{id}', 'DepartmentController@detail')->name('departmentDetail');
-
-
-Route::get('/user/{id}', 'UserController@details')->middleware('auth')->name('userDetail');
-
-Route::get('user/image/{user_id}', 'UserController@getUserImage')->name('getUserImage');
-
-Route::get('/user/{id}', 'UserController@details')->name('userDetail');
-
-Route::get('/user/edit/{id}', 'UserController@edit')->name('editUser');
 
 
 Route::get('/admin', 'DashboardController@getIndex')->middleware('auth')->middleware('admin')->name('homeAdmin');
-
-Route::post('/user/edit/{id}', 'UserController@update')->name('updateUser');
 
 Route::post('admin/user/{id}/setadmin', 'UserController@setUserAsAdmin')->middleware('auth')->middleware('admin')->name('setUserAsAdmin');
 
