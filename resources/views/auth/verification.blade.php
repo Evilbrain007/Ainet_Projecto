0146c4eace2ac6f7d146d0f84b9eeb1049c12b9c
Daniel Gomes <gomesdanielm@gmail.com>
2017-05-25 15:32:54 +0100

    Mudar inglês para português no ecrã de envio de email de registo.

--- resources/views/auth/verification.blade.php
+++ resources/views/auth/verification.blade.php
@@ -4,9 +4,9 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Registration</div>
+                <div class="panel-heading">Registo de utilizador</div>
                 <div class="panel-body">
-                    You have successfully registered. An email is sent to you for verification.
+                    Registou-se com sucesso. Irá receber um email para activar a conta.
                 </div>
             </div>
         </div>
